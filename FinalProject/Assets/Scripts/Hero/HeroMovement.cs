c37e0b95f577ba94cfd61cf6410f85d3bf299110
masterkusok <fedyasetrakov@gmail.com>
2023-04-03 23:41:42 +0300

    Импорт ассетов, стартовые приготовления

---
+++ FinalProject/Assets/Scripts/Hero/HeroMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    
+    public void Turn(Vector2 direction)
+    {
+
+    }
+}
